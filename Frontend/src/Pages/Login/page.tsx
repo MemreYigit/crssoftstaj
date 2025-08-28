f0a2d4539333b128ad486625b10120afa1ba5eb4
Mustafa Emre Yigit <memrex14@gmail.com>
2025-08-28 13:57:54 +0300

    Register
    Register özelliği getirildi. Login sayfasında ikisi de var.

--- Frontend/src/Pages/Login/page.tsx
+++ Frontend/src/Pages/Login/page.tsx
@@ -1,44 +1,76 @@
 import React, { useState } from "react";
-import './page.css';
+import "./page.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const api = axios.create({
-  baseURL: "",            
-  withCredentials: true,  // important for cookie
+  baseURL: "",
+  withCredentials: true,
 });
 
+type Action = "login" | "register";
 
 const Login: React.FC = () => {
+  const [action, setAction] = useState<Action>("login");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [error, setError] = useState<string | null>(null);
+
   const navigate = useNavigate();
 
-  const handleSubmit = async () => {
+  const loginSubmit = async () => {
+    await api.post("/login", { email, password });
+    setError(null);
+    navigate("/");
+  };
+
+  const registerSubmit = async () => {
+    await api.post("/register", { email, password, name });
+    setError(null);
+    setAction("login");
+    navigate("/login");
+  };
+
+  const handleSubmit = async (e:React.FormEvent) => {
     try {
-      await api.post("/login", { email, password });  
-      setError(null);
-      navigate("/");
-    } catch (err: any) {
-      setError(err?.response?.data ?? "Login failed.");
+      e.preventDefault();
+      if (action === "login") {
+        await loginSubmit();
+      } 
+      else {
+        await registerSubmit();
+      }
+    } 
+    catch (err: any) {
+      setError(err.response.data);
     }
   };
 
   return (
     <div className="login-container">
       <div className="login-body">
-        
         <div className="login-header">
           <h2 className="login-title">GameX</h2>
         </div>
 
         {error && <div className="error-message">{error}</div>}
 
-        <div className="login-inputs">
+        <form className="login-inputs" onSubmit={handleSubmit}>
+          {action === "register" && (
+            <div className="login-input">
+              <i className="fa-solid fa-circle-user"/>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          )}
 
           <div className="login-input">
-            <i className="fas fa-envelope"></i>
+            <i className="fas fa-envelope"/>
             <input
               type="email"
               placeholder="Email"
@@ -48,7 +80,7 @@ const Login: React.FC = () => {
           </div>
 
           <div className="login-input">
-            <i className="fas fa-lock"></i>
+            <i className="fas fa-lock"/>
             <input
               type="password"
               placeholder="Password"
@@ -56,11 +88,28 @@ const Login: React.FC = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-        </div>
+        </form>
 
-        <div className="login-submit" onClick={handleSubmit}>
-          Login
-        </div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <button className="login-submit">
+            {action === "login" ? "Login" : "Register"}
+          </button>
+
+          <div className="login-toggle">
+            {action === "login" && (
+              <button onClick={() => {setAction("register"); setError(null)}}>
+                Don’t have an account?
+              </button>
+            )}
+            {action === "register" && (
+              <button onClick={() => {setAction("login"); setError(null)}}>
+                Already have an account? 
+              </button>
+            )}
+
+          </div>
+        </form>
+
       </div>
     </div>
   );
